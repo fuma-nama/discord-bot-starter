93cb36f32311e4cfbd0a6828b3ed4ba36077db6d
SonMooSans <god63820869@gmail.com>
2023-01-30 17:50:13 +0800

    Upgrade to discord-fp@1.0.1

--- index.ts
+++ index.ts
@@ -1,23 +1,16 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { start } from "discord-fp";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
 import "dotenv/config";
 
 //store your token in environment variable or put it here
 const token = process.env["TOKEN"];
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-function resolveDirs(...names: string[]): string[] {
-    const currentDir = dirname(import.meta.url);
-
-    return names.map((name) => fileURLToPath(join(currentDir, name)));
-}
-
 client.on("ready", () => {
     console.log(`Logged in as ${client.user?.tag}!`);
+
     start(client, {
-        dir: resolveDirs("./commands"),
+        load: ["./commands"],
     });
 });
 
